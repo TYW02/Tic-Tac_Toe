21dddfb124f7f8af00254ebbc301679ac0598737
unknown <02yuanweic2@gmail.com>
2022-05-31 20:53:40 +0800

    Renamed Files and Added Comments

--- server.py
+++ server.py
@@ -6,17 +6,20 @@ class TicTacToe:
 
     def __init__(self):
         self.board = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
+        # X starts the turn, X is also the one who hosts the game
         self.turn = "X"
         self.you = "X"
         self.opponent = "O"
         self.winner = None
         self.game_over = False
 
+        # Turn counter if counter is 9 it means it is a draw
         self.counter = 0
 
     def host_game(self, host, port):
         server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server.bind((host, port))
+        # Listening for 1 connection
         server.listen(1)
 
         client, addr = server.accept()
@@ -39,15 +42,19 @@ class TicTacToe:
         while not self.game_over:
             # If its your turn...
             if self.turn == self.you:
+                self.print_board()
                 move = input("Enter Your Move (Row,Col): ")
                 if self.check_valid_move(move.split(',')):
                     client.send(move.encode('utf-8'))
+                    # Apply the move with your symbol, then change turn
                     self.apply_move(move.split(','), self.you)
                     self.turn = self.opponent
+
 
                 else:
                     print("Invalid Move")
             else:
+                # 1024 bytes
                 data = client.recv(1024)
                 if not data:
                     break
@@ -60,6 +67,7 @@ class TicTacToe:
         if self.game_over:
             return
         self.counter += 1
+        # Player is the symbol
         self.board[int(move[0])][int(move[1])] = player
         self.print_board()
         if self.check_if_won():
@@ -67,7 +75,7 @@ class TicTacToe:
                 print("You WON!")
                 exit()
             elif self.winner == self.opponent:
-                print("You LOSE!")
+                print("You LOST!")
                 exit()
         else:
             if self.counter == 9:
@@ -75,25 +83,35 @@ class TicTacToe:
                 exit()
 
     def check_valid_move(self, move):
+        # If board at this position is empty
         return self.board[int(move[0])][int(move[1])] == " "
 
     def check_if_won(self):
         for row in range(3):
+            # Checks for row winning condition
+            # and if the symbol is not the default symbol
             if self.board[row][0] == self.board[row][1] == self.board[row][2] != " ":
                 self.winner = self.board[row][0]
                 self.game_over = True
                 return True
 
         for col in range(3):
+            # Checks for column winning condition
+            # and if the symbol is not the default symbol
             if self.board[0][col] == self.board[1][col] == self.board[2][col] != " ":
                 self.winner = self.board[0][col]
                 self.game_over = True
                 return True
 
+        # Check for diagonal winning condition (from top left to bottom right)
+        # and if the symbol is not the default symbol
         if self.board[0][0] == self.board[1][1] == self.board[2][2] != " ":
             self.winner = self.board[0][0]
             self.game_over = True
             return True
+
+        # Check for diagonal winning condition (from top right to bottom left)
+        # and if the symbol is not the default symbol
         if self.board[0][2] == self.board[1][1] == self.board[2][0] != " ":
             self.winner = self.board[0][2]
             self.game_over = True
@@ -103,9 +121,11 @@ class TicTacToe:
     def print_board(self):
         for row in range(3):
             print(" | ".join(self.board[row]))
+            # if row is not the last row print the separator
             if row != 2:
                 print("-----------")
 
 
 game = TicTacToe()
-game.connect_to_game("localhost", 9999)
+# Change to your/server IP address
+game.host_game("localhost", 9999)
